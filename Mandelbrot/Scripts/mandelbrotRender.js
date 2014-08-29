6e6771dbef9a0bb2c12399d0e37ac4af2561657d
Martin Åhlin <martin.aahlin@gmail.com>
2014-08-29 16:23:25 +0200

    partition timing

--- Mandelbrot/Scripts/mandelbrotRender.js
+++ Mandelbrot/Scripts/mandelbrotRender.js
@@ -31,7 +31,7 @@ $(document).ready(
 
         //Set event handlers
         $("#renderMandelbrot").click(function () {
-            mandelbrot.fractals.push(new Fractal(mandelbrot.min_c_re.val(), mandelbrot.min_c_im.val(),
+            mandelbrot.fractals.push(new Fractal(mandelbrot.fractals.length, mandelbrot.min_c_re.val(), mandelbrot.min_c_im.val(),
             mandelbrot.max_c_re.val(), mandelbrot.max_c_im.val(), mandelbrot.x.val(), mandelbrot.y.val(),
              mandelbrot.inf_n.val(), mandelbrot.p_threads.val()));
             return false;
@@ -39,7 +39,10 @@ $(document).ready(
 
     });
 
-function Fractal(min_c_re, min_c_im, max_c_re, max_c_im, x, y, inf_n, p_threads) {
+function Fractal(id, min_c_re, min_c_im, max_c_re, max_c_im, x, y, inf_n, p_threads) {
+    //Class members
+    var partitionCounter = 1;
+    var startDate = new Date();
 
     //Help functions
     var checkZero = function (number) {
@@ -49,16 +52,35 @@ function Fractal(min_c_re, min_c_im, max_c_re, max_c_im, x, y, inf_n, p_threads)
         return number;
     }
 
-    var img = $("<img />").attr('src', 'http://somedomain.com/image.jpg')
-    .load(function () {
-        if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-            alert('broken image!');
-        } else {
-            $("#something").append(img);
-        }
-    });
+    var renderTime = function (date) {
+        return checkZero(date.getHours().toString()) + ":" + checkZero(date.getMinutes().toString()) + ":" +
+            checkZero(date.getSeconds().toString());
+    }
 
-    //Apply modulo 256
+    var loadNewPartition = function (start_x, end_x) {
+        var img = $("<img />")
+            .attr("style", "left: " + start_x + "px")
+            .attr("src", "/mandelbrot?min_c_re=" + min_c_re + "&min_c_im=" + min_c_im +
+            "&max_c_re=" + max_c_re + "&max_c_im=" + max_c_im + "&x=" + x + "&y=" + y +
+            "&inf_n=" + inf_n + "&start_x=" + start_x + "&end_x=" + end_x )
+        .load(function () {
+            if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+                console.log('could not render fractal');
+            } else {
+                $("#mandelWrapper #fractalId_" + id + " .partialImageWrapper").append(img);
+                var endDate = new Date();
+                var endTimeLabel = "<p><span class='fractalInputLabel'>Partition " + partitionCounter +
+                    " finished at:</span><span>" + renderTime(endDate) + "</span></p>";
+                $("#mandelWrapper #fractalId_" + id).append(endTimeLabel);
+                var endTimeDifference = "<p><span class='fractalInputLabel'>Elapsed time:</span><span>" +
+                    renderTime(endDate - startDate) + "</span></p>"; //Fixa detta!!!!
+                $("#mandelWrapper #fractalId_" + id).append(endTimeDifference);
+                partitionCounter += 1;
+            }
+        });
+    };
+
+    //Apply modulo 256 to input
     x = x - x % 256;
     y = y - y % 256;
 
@@ -75,24 +97,18 @@ function Fractal(min_c_re, min_c_im, max_c_re, max_c_im, x, y, inf_n, p_threads)
     var yLabel = "<p><span class='fractalInputLabel'>Height:</span><span>" + y + "</span></p>";
     var inf_nLabel = "<p><span class='fractalInputLabel'>Max iterations:</span><span>" + inf_n + "</span></p>";
     var p_threadsLabel = "<p><span class='fractalInputLabel'>Parallell threads:</span><span>" + p_threads + "</span></p>";
-    var startDate = new Date();
-    var startTime = "<p><span class='fractalInputLabel'>Started:</span><span>" +
-        checkZero(startDate.getHours().toString()) + ":" + checkZero(startDate.getMinutes().toString()) + ":" +
-        checkZero(startDate.getSeconds().toString()) + "</span></p>";
-    var imageElement = "<div class='partialImageWrapper'>";
+    var startTimeLabel = "<p><span class='fractalInputLabel'>Started:</span><span>" + renderTime(startDate) + "</span></p>";
+    var imageElement = "<div class='partialImageWrapper'></div>";
     for (var i = 0; i < p_threads - 1; i++) {
         //Renders all parts except last one
-        imageElement += "<img class='partialImage' style='left: " + (i * partitionWidth) + "px' src='/mandelbrot?min_c_re=" +
-            min_c_re + "&min_c_im=" + min_c_im + "&max_c_re=" + max_c_re + "&max_c_im=" + max_c_im + "&x=" + x + "&y=" + y +
-        "&inf_n=" + inf_n + "&start_x=" + (i * partitionWidth) + "&end_x=" + ((i + 1) * partitionWidth) + "' />";
+        loadNewPartition(i * partitionWidth, (i + 1) * partitionWidth);
     }
     //Last image partition compensates for rounding.
-    imageElement += "<img class='partialImage' style='left: " + (partitionWidth * (p_threads - 1)) + "px' src='/mandelbrot?min_c_re=" +
-        min_c_re + "&min_c_im=" + min_c_im + "&max_c_re=" + max_c_re + "&max_c_im=" + max_c_im + "&x=" + x + "&y=" + y +
-        "&inf_n=" + inf_n + "&start_x=" + (partitionWidth * (p_threads - 1)) + "&end_x=" + x + "' />";
-    imageElement += "</div>";
+    loadNewPartition(partitionWidth * (p_threads - 1), x);
+    //Styling
     var wrapperHeight = y + 20 > 340 ? y + 20 : 340;
-    $("#mandelWrapper").prepend("<div class='fractalSet' style='height: " + wrapperHeight + "px'>" + header +
+    //Prepend to DOM
+    $("#mandelWrapper").prepend("<div id='fractalId_" + id + "' class='fractalSet' style='min-height: " + wrapperHeight + "px'>" + header +
         min_c_reLabel + min_c_imLabel + max_c_reLabel + max_c_imLabel +
-        xLabel + yLabel + inf_nLabel + p_threadsLabel + startTime + imageElement + "</div>");
+        xLabel + yLabel + inf_nLabel + p_threadsLabel + startTimeLabel + imageElement + "</div>");
 }
